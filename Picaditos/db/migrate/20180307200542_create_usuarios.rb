4dc8e6b2e712a249fbc40b9d772f37fa2ba3d4da
Diego Fernando Chacon Arango <dfchacona@unal.edu.co>
2018-03-07 20:12:11 +0000

    Primeros modelos

---
+++ Picaditos/db/migrate/20180307200542_create_usuarios.rb
@@ -0,0 +1,16 @@
+class CreateUsuarios < ActiveRecord::Migration[5.1]
+  def change
+    create_table :usuarios do |t|
+      t.string :tipo_de_documento
+      t.string :nombres
+      t.string :apellidos
+      t.date :fecha_nacimiento
+      t.integer :telefono
+      t.string :correo_electronico
+      t.string :num_deportes
+      t.integer :calificacion
+
+      t.timestamps
+    end
+  end
+end
