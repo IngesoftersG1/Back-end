1d4657db3e149f0a7406cb2b19eb506c1b0609c8
dieguischa <dfchacona@unal.edu.co>
2018-03-13 18:32:09 -0500

    user validation

--- Picaditos/app/models/usuario.rb
+++ Picaditos/app/models/usuario.rb
@@ -1,13 +1,13 @@
 
 class Usuario < ApplicationRecord
-    validates_with EmailValidator
     self.primary_key = "user_name"
     validates :user_name, uniqueness: true
     validates :nombres, presence: {with: true, message: "Ingrese el nombre"}, format: { with: /\A[a-zA-Z]+\z/,
     message: "Solo se permiten letras" }
     validates :apellidos, presence: {with: true, message: "Ingrese el apellido"}, format: { with: /\A[a-zA-Z]+\z/,
     message: "Solo se permiten letras" }
+    validates :correo_electronico , format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,    message: "Solo se permiten letras" }
     validates :fecha_nacimiento, presence: {with: true, message: "Ingrese el nombre"}
-    validates :correo_electronico, presence: {with: true, message: "Ingrese el nombre"}, email: true
+    
     
 end
