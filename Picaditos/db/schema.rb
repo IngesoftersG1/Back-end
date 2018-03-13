2b711cf05c553a57fe47c9a2510a95cd2432cda3
dieguischa <dfchacona@unal.edu.co>
2018-03-13 18:54:02 -0500

    Foreign key correction

--- Picaditos/db/schema.rb
+++ Picaditos/db/schema.rb
@@ -20,10 +20,10 @@ ActiveRecord::Schema.define(version: 20180312204333) do
     t.text "descripcion"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "usuario_id"
+    t.string "user_name"
     t.integer "tablon_id"
     t.index ["tablon_id"], name: "index_anuncios_on_tablon_id"
-    t.index ["usuario_id"], name: "index_anuncios_on_usuario_id"
+    t.index ["user_name"], name: "index_anuncios_on_user_name"
   end
 
   create_table "canchas", force: :cascade do |t|
@@ -32,21 +32,21 @@ ActiveRecord::Schema.define(version: 20180312204333) do
     t.integer "calificacion"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "usuario_id"
+    t.string "user_name"
     t.integer "ubicacion_id"
     t.index ["ubicacion_id"], name: "index_canchas_on_ubicacion_id"
-    t.index ["usuario_id"], name: "index_canchas_on_usuario_id"
+    t.index ["user_name"], name: "index_canchas_on_user_name"
   end
 
   create_table "deportes", force: :cascade do |t|
     t.text "descripcion"
     t.integer "min_jugadores"
     t.integer "ubicacion_id"
-    t.integer "usuario_id"
+    t.string "user_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["ubicacion_id"], name: "index_deportes_on_ubicacion_id"
-    t.index ["usuario_id"], name: "index_deportes_on_usuario_id"
+    t.index ["user_name"], name: "index_deportes_on_user_name"
   end
 
   create_table "equipos", force: :cascade do |t|
@@ -55,10 +55,10 @@ ActiveRecord::Schema.define(version: 20180312204333) do
     t.integer "calificacion"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "usuario_id"
+    t.string "user_name"
     t.integer "deporte_id"
     t.index ["deporte_id"], name: "index_equipos_on_deporte_id"
-    t.index ["usuario_id"], name: "index_equipos_on_usuario_id"
+    t.index ["user_name"], name: "index_equipos_on_user_name"
   end
 
   create_table "estadisticas", force: :cascade do |t|
@@ -73,12 +73,12 @@ ActiveRecord::Schema.define(version: 20180312204333) do
 
   create_table "mensajes", force: :cascade do |t|
     t.text "contenido"
-    t.integer "usuario_id"
+   t.string "user_name"
     t.date "fecha"
     t.text "asunto"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["usuario_id"], name: "index_mensajes_on_usuario_id"
+    t.index ["user_name"], name: "index_mensajes_on_user_name"
   end
 
   create_table "partidos", force: :cascade do |t|
@@ -86,13 +86,13 @@ ActiveRecord::Schema.define(version: 20180312204333) do
     t.integer "ubicacion_id"
     t.integer "equipo_id"
     t.integer "deporte_id"
-    t.integer "usuario_id"
+   t.string "user_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["deporte_id"], name: "index_partidos_on_deporte_id"
     t.index ["equipo_id"], name: "index_partidos_on_equipo_id"
     t.index ["ubicacion_id"], name: "index_partidos_on_ubicacion_id"
-    t.index ["usuario_id"], name: "index_partidos_on_usuario_id"
+    t.index ["user_name"], name: "index_partidos_on_user_name"
   end
 
   create_table "tablons", force: :cascade do |t|
@@ -108,11 +108,11 @@ ActiveRecord::Schema.define(version: 20180312204333) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "deporte_id"
-    t.integer "usuario_id"
+   t.string "user_name"
     t.integer "ubicacion_id"
     t.index ["deporte_id"], name: "index_torneos_on_deporte_id"
     t.index ["ubicacion_id"], name: "index_torneos_on_ubicacion_id"
-    t.index ["usuario_id"], name: "index_torneos_on_usuario_id"
+    t.index ["user_name"], name: "index_torneos_on_user_name"
   end
 
   create_table "ubicacions", force: :cascade do |t|
