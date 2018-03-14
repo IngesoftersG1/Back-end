c403502b035d003608c639a0a03d3b0eb95431e2
Diego Fernando Chacon Arango <dfchacona@unal.edu.co>
2018-03-14 15:31:29 +0000

    Changed to api mode

--- Picaditos/config/application.rb
+++ Picaditos/config/application.rb
@@ -8,9 +8,10 @@ Bundler.require(*Rails.groups)
 
 module Picaditos
   class Application < Rails::Application
+    config.api_only  = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+  
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
