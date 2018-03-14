d8ab3973407e67bf834869a3665e2c8fe321199b
dieguischa <dfchacona@unal.edu.co>
2018-03-13 19:35:19 -0500

    Faker gem

--- Picaditos/db/seeds.rb
+++ Picaditos/db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+100.times do |row|
+    Ubicacion.create(
+    calle_principal: Faker::Address.building_number,     
+    calle_secundaria: Faker::Address.building_number,
+    ciudad: Faker::Address.city
+    )
+end
+
+
+100.times do |row|
+  Usuario.create(
+  user_name: Faker::LeagueOfLegends.champion,    
+  nombres: Faker::Name.first_name,
+  apellidos: Faker::Name.last_name,
+  correo_electronico: Faker::Internet.email, 
+  fecha_nacimiento: Faker::Date.backward(23_725),
+  telefono: Faker::PhoneNumber.subscriber_number,
+  ubicacion_id: Faker::Number.number(3)
+  )
+end
+
