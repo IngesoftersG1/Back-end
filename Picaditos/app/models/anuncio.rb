615486c9931c2d74a235553fdec2a74e06bdfbf0
esdpename <esdpename@unal.edu.co>
2018-03-15 04:23:41 +0000

    correccion de errores

--- Picaditos/app/models/anuncio.rb
+++ Picaditos/app/models/anuncio.rb
@@ -1,2 +1,3 @@
 class Anuncio < ApplicationRecord
+    validates :descripcion, presence: {with: true, message: "El cuerpo del anuncio no puede estar vacío"}
 end
--- Picaditos/app/models/deporte.rb
+++ Picaditos/app/models/deporte.rb
@@ -1,2 +1,3 @@
 class Deporte < ApplicationRecord
+    validates :nombre, presence: {with: true, message: "Ingrese el nombre del deporte"}
 end
