a6906a40ae98dce95329f87e341e7ac2594855dd
dieguischa <dfchacona@unal.edu.co>
2018-04-10 22:59:17 -0500

    Added back-end authentication

--- Picaditos/app/controllers/application_controller.rb
+++ Picaditos/app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
   include ActionController::MimeResponds
   include Knock::Authenticable
-  ##protect_from_forgery with: :exception
+  ##protect_from_forgery with: :exception 
   
 end
