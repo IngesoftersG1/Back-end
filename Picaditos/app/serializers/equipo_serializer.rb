89335d57a7ccd86e22023a8c2c731b2f1cb35218
dieguischa <dfchacona@unal.edu.co>
2018-05-01 19:32:00 -0500

    Improved Serializers

--- Picaditos/app/serializers/equipo_serializer.rb
+++ Picaditos/app/serializers/equipo_serializer.rb
@@ -1,5 +1,6 @@
 class EquipoSerializer < ActiveModel::Serializer
   attributes :nombre, :nivel, :capitan_name, :calificacion, :deporte_id
-
+  has_many :users 
+  has_one :deporte
 
 end
--- Picaditos/app/serializers/torneo_serializer.rb
+++ Picaditos/app/serializers/torneo_serializer.rb
@@ -1,5 +1,5 @@
 class TorneoSerializer < ActiveModel::Serializer
   attributes :fecha, :premio, :organizador_name , :nombre
-
+  has_many :equipos
 
 end
